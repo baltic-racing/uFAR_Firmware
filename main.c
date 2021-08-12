1ac24f42d06df3abba99574e93e41d25cb49a549
Riddick-ulous <kukasdeeken@outlook.com>
2021-08-12 19:34:27 +0200

    added launch controll, autoshift, and sort a 2step, TC is WIP

--- main.c
+++ main.c
@@ -5,13 +5,6 @@
  * Author : Ole Hannemann
  */ 
 
-#define SHIFT_UP swc_databytes[3]
-#define SHIFT_DOWN swc_databytes[2]
-#define BUTTON_LEFT swc_databytes[4]
-#define BUTTON_RIGHT swc_databytes[5]
-#define LEFT_ENCODER swc_databytes[1]
-#define RIGHT_ENCODER swc_databytes[0]
-
 #include <avr/io.h>
 #include "canlib.h"
 #include "misc_functions.h"
@@ -22,6 +15,15 @@
 #include "Fuse_CTRL.h"
 #include <avr/interrupt.h>
 
+#define SHIFT_UP swc_databytes[3]
+#define SHIFT_DOWN swc_databytes[2]
+#define BUTTON_LEFT swc_databytes[4]
+#define BUTTON_RIGHT swc_databytes[5]
+#define LEFT_ENCODER swc_databytes[1]
+#define RIGHT_ENCODER swc_databytes[0]
+
+uint8_t swc_databytes[8];
+
 extern unsigned long sys_time;
 volatile unsigned long time_old = 0;
 volatile uint8_t time_old_100 = 0;
@@ -30,13 +32,17 @@ volatile uint16_t fan_deadtime = 75;
 extern volatile uint8_t Blipper_Enable;
 extern volatile uint8_t Anti_Blipper_Enable;
 extern uint16_t fan_time;
-uint8_t LC_Active = 0;
+uint8_t LC_State = 0;
 double BP = 0;
 uint8_t APPS1 = 0;
 uint8_t APPS2 = 0;
 extern LC_Launch;
 extern LC_Ready;
-extern EMULC_Active;
+double GPS_Speed = 0;
+uint8_t Flatshift_Active;
+extern uint8_t Launch_Flatshift_Active;
+extern uint8_t Shift_Flatshift_Active;
+extern uint8_t gear_up_autmatic;
 
 int main(void)
 {
@@ -50,7 +56,6 @@ int main(void)
 	can_SWC_mob.mob_id = 0x100;
 	can_SWC_mob.mob_idmask = 0xfff;
 	can_SWC_mob.mob_number = 0;
-	uint8_t swc_databytes[8];
 	
 	struct CAN_MOB can_CMC_mob;
 	can_CMC_mob.mob_id = 0x200;
@@ -71,9 +76,9 @@ int main(void)
 	uint8_t ecu1_databytes[8];
 	
 	struct CAN_MOB can_DIC_mob;
-	can_ecu1_mob.mob_id = 0x202;
-	can_ecu1_mob.mob_idmask = 0xffff;
-	can_ecu1_mob.mob_number = 4;
+	can_DIC_mob.mob_id = 0x202;
+	can_DIC_mob.mob_idmask = 0xffff;
+	can_DIC_mob.mob_number = 4;
 	uint8_t DIC_databytes[8];
 	
 	struct CAN_MOB can_ETC_mob;
@@ -93,7 +98,19 @@ int main(void)
 	can_EMUSW_mob.mob_idmask = 0xffff;
 	can_EMUSW_mob.mob_number = 7;
 	uint8_t EMUSW_databytes[8];	
-		
+	
+	struct CAN_MOB can_DL3_mob;
+	can_DL3_mob.mob_id = 0x797;
+	can_DL3_mob.mob_idmask = 0xffff;
+	can_DL3_mob.mob_number = 8;
+	uint8_t DL3_databytes[8];
+	
+	struct CAN_MOB can_CMC2_mob;
+	can_CMC2_mob.mob_id = 0x211;
+	can_CMC2_mob.mob_idmask = 0;
+	can_CMC2_mob.mob_number = 9;
+	uint8_t cmc2_databytes[8];	
+
 	volatile uint8_t gear = 10;
 	
 	sei();
@@ -105,6 +122,14 @@ int main(void)
 			time_old = sys_time;
 			time_old_100++;
 			
+			if (Launch_Flatshift_Active == TRUE || Shift_Flatshift_Active == TRUE)
+			{
+				Flatshift_Active == TRUE;
+			}
+			else{
+				Flatshift_Active == FALSE;
+			}
+			
 			gear = gear_read(adc_read());
 			//send some additional data to make out if the gear gets transmitted correctly
 			cmc_databytes[0] = gear;
@@ -114,38 +139,44 @@ int main(void)
 			cmc_databytes[4] = get_fuse_status()>>8;//get MSB	
 			cmc_databytes[5] = Blipper_Enable;
 			cmc_databytes[6] = Anti_Blipper_Enable;
-			cmc_databytes[7] = gear+1;
-					
+			cmc_databytes[7] = Flatshift_Active;
+			
+			cmc2_databytes[0] = gear_up_autmatic;
+			cmc2_databytes[1] = 0;
+			cmc2_databytes[2] = 0;
+			cmc2_databytes[3] = 0;
+			cmc2_databytes[4] = 0;
+			cmc2_databytes[5] = 0;
+			cmc2_databytes[6] = 0;
+			cmc2_databytes[7] = 0;			
+		
 			can_tx(&can_CMC_mob, cmc_databytes);
-			can_tx(&can_EMUSW_mob, EMUSW_databytes);
+			can_tx(&can_CMC2_mob, cmc2_databytes);
 			
 			can_rx(&can_SWC_mob, swc_databytes);
 			can_rx(&can_ecu0_mob, ecu0_databytes);
 			can_rx(&can_ecu1_mob, ecu1_databytes);
 			can_rx(&can_DIC_mob, DIC_databytes);
+			can_rx(&can_ETC_mob, ETC_databytes);
+			can_rx(&can_DL3_mob, DL3_databytes);
+			can_rx(&can_SHL_mob, SHL_databytes);
 			
 			rpm = ecu0_databytes[1]<<8 | ecu0_databytes[0];
-			LC_Active = DIC_databytes[0];
-			BP = (SHL_databytes[3] <<8|SHL_databytes[2])/10; //gets Brake Pressure in 0.1 Bar
+			LC_State = DIC_databytes[0];
+			BP = (double)(SHL_databytes[3] <<8|SHL_databytes[2])/10; //gets Brake Pressure in 0.1 Bar
 			APPS1 = ETC_databytes[0];
 			APPS2 = ETC_databytes[1];
+			GPS_Speed = (double) (DL3_databytes[7] <<8 | DL3_databytes[6]);
 			
-			//IF we are in the Launch control procedure we want to tell the ecumaster to switch 2step on via the CAN SW1 bit
-			if (EMULC_Active = TRUE;)
-			{
-				EMUSW_databytes[5] = 1;
-			}
-			else
-			{
-				EMUSW_databytes[5] = 0;
-			}
-
+			
+			
 			calculate_locktimes();
-			shift_control(SHIFT_UP,SHIFT_DOWN,gear,rpm);
-			clutch_control(BUTTON_LEFT||BUTTON_RIGHT,LEFT_ENCODER+1,gear,LC_Active,APPS1,APPS2,BP);
+			shift_control(SHIFT_UP,SHIFT_DOWN,gear,rpm,LC_State, GPS_Speed);
+			// -> is now in the interrupt becouse we need to control the Launch control stuff with 1khz clutch_control(BUTTON_RIGHT|BUTTON_LEFT,LEFT_ENCODER+1,gear,LC_State,APPS1,APPS2,BP);
 			fuelpump_CTRL(rpm);
-			
+		
 		}
+		
 		//100ms loop 10Hz
 		if(time_old_100 >=10){
 			time_old_100 = 0;
@@ -168,4 +199,5 @@ int main(void)
 		
 	}
 }
+	
 
--- misc_functions.c
+++ misc_functions.c
@@ -1,4 +1,4 @@
-/*
+/*uint8_t
  * misc_functions.c
  *
  * Created: 05.04.2019 18:51:39
@@ -6,8 +6,32 @@
  * last change: 21.02.2021
  *  Author: Lukas Deeken
  */ 
+
 #include "misc_functions.h"
+#include "servo_functions.h"
+
 volatile unsigned long sys_time = 0;
+uint8_t on_time;
+unsigned long systime_PWM = 0;
+uint8_t PWM_on = 0;
+uint16_t esum = 0;
+extern uint8_t Flatshift_Active;
+extern uint16_t rpm;
+
+extern uint8_t gear;
+extern uint8_t LC_State;
+extern uint8_t APPS1;
+extern uint8_t APPS2;
+extern double BP;
+
+extern uint8_t swc_databytes[8];
+
+#define SHIFT_UP swc_databytes[3]
+#define SHIFT_DOWN swc_databytes[2]
+#define BUTTON_LEFT swc_databytes[4]
+#define BUTTON_RIGHT swc_databytes[5]
+#define LEFT_ENCODER swc_databytes[1]
+#define RIGHT_ENCODER swc_databytes[0]
 
 void port_config(){
 	DDRA = 0;												//All either generic Digital Inputs or Fuse Inputs
@@ -17,6 +41,7 @@ void port_config(){
 	DDRE = 0 | (1<<PE3) | (1<<PE4);							//PE3 and PE4 as FAN_CTRL outputs otherwise Fuse Inputs
 	DDRF = 0;												//mark analog inputs on Port F as input
 }
+
 void sys_tick(){
 	PORTC ^= 1<<PC2; //toggling the Status of PC2 to make the Heart LED blink every SYStick
 }
@@ -24,7 +49,7 @@ void sys_tick(){
 void sys_timer_config(){
 	
 	//8 bit Timer 0 configuration
-	//ctc mode and 64 as prescaler for 8Mhz Quartz, for 16Mhz use 64
+	//ctc mode and 32 as prescaler for 8Mhz Quartz, for 16Mhz use 64
 	TCCR0A = 0 | (1<<WGM01) | (1<<CS01) | (1<<CS00);
 	TIMSK0 = 0 | (1<<OCF0A);	//compare interrupt enable
 	OCR0A = 250-1;				// compare value for 1ms;
@@ -44,8 +69,10 @@ void fuelpump_CTRL(uint16_t rpm){
 
 //ISR for Timer 0 compare interrupt
 ISR(TIMER0_COMP_vect){
-	
+	//1ms loop
 	sys_time++; //system time generation
 	//for every time the timer equals 249 an interrupt is generated resulting in invreasing the SYStime
-
+	
+	clutch_control(BUTTON_RIGHT|BUTTON_LEFT,LEFT_ENCODER+1,gear,LC_State,APPS1,APPS2,BP);
+	
 }
