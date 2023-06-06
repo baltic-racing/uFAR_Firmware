5a632f19a9f19cb58f5da0cd958c9517985c6b6f
Lukas <48808297+Riddick-ulous@users.noreply.github.com>
2023-06-06 16:16:57 +0200

    Current Code on Vehicle
    Uploaded from tm6473nb on electronics laptop

---
+++ Debug/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+adc_functions.c
+
+canlib.c
+
+fan_control.c
+
+Fuse_CTRL.c
+
+gear_read.c
+
+main.c
+
+misc_functions.c
+
+servo_functions.c
+
